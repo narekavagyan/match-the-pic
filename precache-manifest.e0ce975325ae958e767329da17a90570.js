f25ef717cefb35e645f172989b2b4bed9f937408
avagyannarek <avagyannarek1999@gmail.com>
2019-11-23 08:09:12 +0400

    Updates

---
+++ precache-manifest.e0ce975325ae958e767329da17a90570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fda1fd8a59e36ceafb84a91c6ca9a9c",
+    "url": "/match-the-pic/index.html"
+  },
+  {
+    "revision": "4874a871d2f5a81a474a",
+    "url": "/match-the-pic/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bc741f544b4d6f87c945",
+    "url": "/match-the-pic/static/js/2.97f5964b.chunk.js"
+  },
+  {
+    "revision": "4874a871d2f5a81a474a",
+    "url": "/match-the-pic/static/js/main.0988f48b.chunk.js"
+  },
+  {
+    "revision": "93920317bdb0c585ce84",
+    "url": "/match-the-pic/static/js/runtime-main.7322fd54.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/match-the-pic/static/media/logo.25bf045c.svg"
+  }
+]);
